a6726be4a446c2b9a9badf2e3fe2a7182a22d93c
Bryan Chapel <bchapel@ebates.com>
2018-05-29 23:25:45 -0500

    Extract SignUp component and add test.

--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -2,10 +2,17 @@ import React from 'react';
 import './SignUp.css';
 
 const signUp = () => {
-    return (<form>
-        <input type="email" autoComplete="email" name="email" placeholder="ernst@hemming.com" />
-        <input type="submit" value="Sign up!"/>
-    </form>);
+    return (
+        <div>
+            <p>
+                Enter your email below to be notified when our beta launches, and to be one of our limited first users.
+            </p>
+            <form>
+                <input type="email" autoComplete="email" name="email" placeholder="ernst@hemming.com" />
+                <input type="submit" value="Sign up!"/>
+            </form>
+        </div>
+    );
 };
 
 export default signUp;
